abfcda7e3f27c63875da9840c4d528f9123f217a
Sheep <danielsheppard2003@gmail.com>
2025-08-25 13:17:19 +0100

    Player Movement, Swim Movement

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplayer = 2.0f;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravityMultiplier = 1.0f;
+
+    [Header("Game Parameters")]
+    [SerializeField] private float diveForce = 0.05f;
+    [SerializeField] private float diveSpeed = 5f;
+
+    [Header("Look Parameters")]
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float upDownLookRange = 80.0f;
+
+    [Header("References")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private InputHandler playerInputHandler;
+
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+    // if sprint triggered is true, then multiply by sprint multiplayer, if not then multiple by 1 (dont change)
+    private float CurrentSpeed => walkSpeed * (playerInputHandler.SprintTriggered ? sprintMultiplayer : 1);
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private Vector3 CalculateWorldDirection()
+    {
+        Vector3 inputDirection = new Vector3(playerInputHandler.MovementInput.x, 0f, playerInputHandler.MovementInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        return worldDirection.normalized;
+    }
+
+    private void HandleJumping()
+    {
+        // if above ground, gravity is normal
+        if (transform.position.y > 0f)
+        {
+            if (characterController.isGrounded)
+            {
+                currentMovement.y = -0.5f;
+
+                if (playerInputHandler.JumpTriggered)
+                {
+                    currentMovement.y = jumpForce;
+                }
+            }
+            else
+            {
+                currentMovement.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+            }
+        }
+        // if just about to go to the top
+        if (transform.position.y < 0f)
+        {
+            if (playerInputHandler.SneakTriggered && currentMovement.y > -diveSpeed)
+            {
+                currentMovement.y += -diveForce;
+            }
+            if (currentMovement.y < 10f)
+            {
+                currentMovement.y += Physics.gravity.y * -gravityMultiplier * Time.deltaTime * 0.5f;
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 worldDirection = CalculateWorldDirection();
+        currentMovement.x = worldDirection.x * CurrentSpeed;
+        currentMovement.z = worldDirection.z * CurrentSpeed;
+
+        HandleJumping();
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    private void ApplyHorizontalRotation(float rotationAmount)
+    {
+        transform.Rotate(0, rotationAmount, 0);
+    }
+
+    private void ApplyVerticalRoation(float rotationAmount)
+    {
+        verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    private void HandleRotation()
+    {
+        float mouseXRotation = playerInputHandler.RotationInput.x * mouseSensitivity;
+        float mouseYRotation = playerInputHandler.RotationInput.y * mouseSensitivity;
+
+        ApplyHorizontalRotation(mouseXRotation);
+        ApplyVerticalRoation(mouseYRotation);
+    }
+}
